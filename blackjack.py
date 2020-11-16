77a69d16a6358b8d539c4f9c4d1e5e61427a5cf6
Parrish Williamson <williamsonparrish@gmail.com>
2020-11-16 16:48:17 -0500

    game setup complete!

--- blackjack.py
+++ blackjack.py
@@ -29,13 +29,118 @@ class Deck:
                 self.all_cards.append(created_card)
 
     def __str__(self):
-        return 'Deck is Ready!'
+        deck_comp = ''
+        for card in self.all_cards:
+            deck_comp += '\n' + card.__str__()
+        return "The deck has: "+deck_comp
 
     def shuffle(self):
-        random.shuffle(self.deck)
+        random.shuffle(self.all_cards)
 
     def deal_one(self):
-        return self.all_cards.pop()
+        single_card = self.all_cards.pop()
+        return single_card
 
-test_deck = Deck()
-print(test_deck)
+# test_deck = Deck()
+# test_deck.shuffle()
+
+class Hand:
+    def __init__(self):
+        self.cards = []
+        self.value = 0
+        self.aces = 0
+
+    def add_card(self,card):
+        self.cards.append(card)
+        self.value += values[card.rank]
+
+        # track aces
+        if card.rank == 'Ace':
+            self.aces += 1
+    
+    def adjust_for_ace(self):
+        
+        # IF TOTAL VALUE > 21 AND I STILL HAVE AN ACE
+        # THAN CHANGE MY ACE TO BE A 1 INSTEAD OF AN 11
+        while self.value > 21 and self.aces:
+            self.value -= 10
+            self.aces -= 1
+
+# test_player = Hand()
+# pulled_card = test_deck.deal_one()
+# print(pulled_card)
+# test_player.add_card(pulled_card)
+# print(test_player.value)
+
+class Chips:
+
+    def __init__(self):
+        self.total = 100
+        self.bet = 0
+
+    def win_bet(self):
+        self.total += self.bet
+
+    def lose_bet(self):
+        self.total -= self.bet
+
+def take_bet(chips):
+    # Ask player for bet
+    # Check if input is an int
+    while True:
+        try:
+            chips.bet = int(input("Please place your bet: "))
+        except:
+            print('There is a type error! Please enter an integer!')
+        else:
+            if chips.bet > chips.total:
+                print("Sorry, you do not have enough chips! You have: {}".format(chips.total))
+            else:
+                break
+
+# take_bet()
+
+def hit(deck,hand):
+    
+    single_card = deck.deal_one()
+    hand.add_card(single_card)
+    hand.adjust_for_ace()
+
+
+def hit_or_stand(deck,hand):
+    global playing
+    
+    ans = input('Would like to Hit or Stand? Enter h or s?: ')
+
+    while playing:
+        if ans[0].lower() == 'h':
+            hit(deck,hand)
+        elif ans[0].lower() == 's':
+            print("Player Stands Dealer's Turn")
+            playing = False
+        else:
+            print("Sorry, I did not understand that, Please enter h or s only!")
+            continue
+        break
+
+# hit_or_stand(test_deck,test_player)
+
+
+def player_busts(player,dealer,chips):
+    print("BUST PLAYER!")
+    chips.lose_bet()
+
+def player_wins(player,dealer,chips):
+    print("PLAYER WINS!")
+    chips.win_bet()
+
+def dealer_busts(player,dealer,chips):
+    print("PLAYER WINS! DEALER BUSTED!")
+    chips.win_bet()
+
+def dealer_wins(player,dealer,chips):
+    print("DEALER WINS!")
+    chips.lose_bet()
+
+def push(player,dealer):
+    print('Dealer and player tie! PUSH')
